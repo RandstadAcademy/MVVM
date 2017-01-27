0c85d49926dc2a1c5375506ca86b262c6fead140
Aula Randstad <Aula Randstad@PC2225>
2017-01-27 10:21:37 +0100

    Aggiunte

---
+++ ViewModel/AssignmentViewModelCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class AssignmentViewModelCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            
+        }
+    }
+}
