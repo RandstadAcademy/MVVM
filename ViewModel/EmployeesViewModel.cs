5f3554a2f708c60634e95ac542787e695eb814bd
Aula Randstad <Aula Randstad@PC2225>
2017-01-27 12:09:28 +0100

    fgfdfdf

---
+++ ViewModel/EmployeesViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class EmployeesViewModel : ViewModelBase
+    {
+        public List<EmployeeModel> Employees { get; set; }
+
+        private EmployeeModel _Employee = null;
+
+        public EmployeeModel Employee
+        {
+            get
+            {
+                if (_Employee == null)
+                {
+                    _Employee = new EmployeeModel();
+                }
+
+
+                return _Employee;
+            }
+
+            set
+            {
+                _Employee = value;
+            }
+
+    }
+}
